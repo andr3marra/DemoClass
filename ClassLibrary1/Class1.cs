1848002e28d76d5a8ad97d03668db0068b60f5c0
DESKTOP-X570\andre <marralopes@gmail.com>
2022-05-25 01:05:05 -0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ClassLibrary1 {
+    public class DemoClass {
+        public string StringProp { get; set; }
+        public int IntProp { get; set; }
+        public uint UIntProp { get; set; }
+    }
+}
